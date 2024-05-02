8eef5fc1b154596fe9e77c396bd4b6e92c710a2a
Laflorentie Nicolas <laflorentie.nicolas@gmail.com>
2024-05-02 16:54:17 +0200

    Add scss notfound page and link backhome

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import "../sass/main.scss";
+
 function NotFound() {
+  const navigate = useNavigate();
+
+  const handleBackHomeClick = () => {
+    navigate("/");
+  };
+
   return (
-    <div>
-      <p>404</p>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
+    <div className="error">
+      <p className="error__number">404</p>
+      <p className="error__text">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <p className="error__backhome" onClick={handleBackHomeClick}>
+        Retourner sur la page d'accueil
+      </p>
     </div>
   );
 }
