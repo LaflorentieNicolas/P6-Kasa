0776483b94263c4dc46823695027a1259ad9246f
Laflorentie Nicolas <laflorentie.nicolas@gmail.com>
2024-05-02 18:05:32 +0200

    Change useNavigate for NavLink in NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,24 +1,17 @@
-import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../sass/main.scss";
 
 function NotFound() {
-  const navigate = useNavigate();
-
-  const handleBackHomeClick = () => {
-    navigate("/");
-  };
-
   return (
     <div className="error">
       <p className="error__number">404</p>
       <p className="error__text">
         Oups! La page que vous demandez n'existe pas.
       </p>
-      <p className="error__backhome" onClick={handleBackHomeClick}>
+      <NavLink to="/" className="error__back-home-page">
         Retourner sur la page d'accueil
-      </p>
+      </NavLink>
     </div>
   );
 }
-
 export default NotFound;
