26dcc311ad2a4dc80c1f922fa1d863b812be69a8
Laflorentie Nicolas <laflorentie.nicolas@gmail.com>
2024-07-30 12:48:29 +0200

    Remove handleCardClick from Card component due to Link usage in Home

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,8 @@ import PropTypes from "prop-types";
 import "../sass/main.scss";
 
 function Card({ cover, title, id, onPress }) {
-  const handleCardClick = () => {
-    if (onPress) {
-      onPress();
-    }
-  };
-
   return (
-    <div className="card" data-id={id} onClick={handleCardClick}>
+    <div className="card" data-id={id} onClick={onPress}>
       <p className="card__title">{title}</p>
       <img className="card__image" src={cover} alt={title} />
     </div>
